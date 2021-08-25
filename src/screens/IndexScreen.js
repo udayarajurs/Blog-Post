cc2a229c66badf3b46047c0cdb0feb86b9b0371c
Udaya raj urs G <Udayarajurs7@gmail.com>
2021-08-25 23:48:35 +0530

    Fill settinng done

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet , View ,  Text } from 'react-native';
+
+const IndexScreen = () => {
+    return(
+        <View>
+            <Text>HI thare.....</Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({});
+
+export default IndexScreen;
