a7177f24609dd7d283141fe2fc4b931074af11bd
udaya raj urs G <Udayarajurs7@gmail.com>
2021-08-26 18:02:45 +0530

    add useContext

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { StyleSheet , View ,  Text } from 'react-native';
+import React , { useContext } from 'react';
+import { StyleSheet , View ,  Text , FlatList , Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
 
 const IndexScreen = () => {
+    const { data , addBlogPost } = useContext (BlogContext)
     return(
         <View>
             <Text>HI thare.....</Text>
+            <Button title="Add Post" onPress={addBlogPost} />
+            <FlatList 
+                data={data}
+                keyExtractor={ BlogPost => BlogPost.title }
+                renderItem={ ({item}) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     );
 };
